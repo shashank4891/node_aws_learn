4e87c6ae33e80d88f63113a3cb3aaaa90523ce9e
Shashank Sinha <132444392+shashank4891@users.noreply.github.com>
2025-02-15 16:20:09 +0530

    added code deploy agent

---
+++ scripts/after_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Installing dependencies..."
+cd /home/ubuntu/app
+npm install  # Install node modules
---
+++ scripts/restart_server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/ubuntu/app
+npm install
+pm2 restart node_aws_learn
